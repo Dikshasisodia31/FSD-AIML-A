2c5923a10fc61fc6f5f15f55d0a4569d9eafe61c
Dikshasisodia31 <dikshasisodia3112@gmail.com>
2025-09-18 09:55:11 +0530

    Learn synchronous programming

---
+++ file_sync.js
@@ -0,0 +1,21 @@
+function login(){
+    console.log("Diksha login successful")
+}
+function getData(){
+    console.log("data is fetched");
+}
+function calculateData(){
+     console.log("Loading......");
+}
+function sendSMS(){
+    console.log("send SMS");
+}
+function logout(){
+    console.log("Logout suggesfully");
+}
+
+login();
+getData();
+calculateData();
+sendSMS();
+logout();
